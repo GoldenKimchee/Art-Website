25abf65bc77d8a342d8eb1f3be231ab5032001ac
Rachel Kim <rachelkim8084@gmail.com>
2021-07-10 21:44:09 -0700

    update navbar

---
+++ actions.js
@@ -0,0 +1,9 @@
+"use strict"
+
+function toggleNav() {
+    document.querySelector(".sidemenu").classList.toggle("sidemenu-open");
+}
+
+function toggleNav2() {
+    document.querySelector(".navbar").classList.toggle("navbar-open");
+}
